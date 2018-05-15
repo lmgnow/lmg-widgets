c5882751e5ef007894a84a5f792054ce5ee78b54
Jeremy Kozan <jeremyakozan@gmail.com>
2018-05-14 20:06:08 -0700

    added submodule

--- lmg-widgets.php
+++ lmg-widgets.php
@@ -3,12 +3,15 @@
  * Plugin Name:   LMG Widgets
  * Plugin URI:    https://github.com/lmgnow/lmg-widgets/
  * Description:   A collection of custom widgets.
- * Version:       1.0.0
+ * Version:       0.1.0
  * Author:        Jeremy Kozan
  * Author URI:    https://www.lmgnow.com/
  */
 
 if ( ! defined( 'WPINC' ) ) die();
+
+//require_once __DIR__ . '/includes/pallazzio-wpghu/pallazzio-wpghu.php';
+//new Pallazzio_WPGHU( __FILE__, 'lmgnow' );
 
 add_action( 'wp_enqueue_scripts', 'lmg_widgets_enqueue' );
 function lmg_widgets_enqueue() {
