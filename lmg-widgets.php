ad8583f5ec5d5914787bcb41be65ea60bce877e6
Jeremy Kozan <jeremyakozan@gmail.com>
2018-05-14 21:07:55 -0700

    fixeed array syntax error

--- lmg-widgets.php
+++ lmg-widgets.php
@@ -3,7 +3,7 @@
  * Plugin Name:   LMG Widgets
  * Plugin URI:    https://github.com/lmgnow/lmg-widgets/
  * Description:   A collection of custom widgets.
- * Version:       1.0.1
+ * Version:       1.0.0
  * Author:        Jeremy Kozan
  * Author URI:    https://www.lmgnow.com/
  */
